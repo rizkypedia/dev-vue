b068f0c580015da5e5df9007a4de1823210578ac
rizkyridwan <rizky.ridwan@trustedshops.de>
2022-06-03 16:17:34 +0200

    Experimentals

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+         <RouterLink to="/random_number">Random Number</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/components/RandomNumbers.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+import { ref } from 'vue';
+
+const randomNumber = ref(Math.random())
+
+</script>
+
+<template>
+    <h2>{{randomNumber}}</h2>
+</template>
